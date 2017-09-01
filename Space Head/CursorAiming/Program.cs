d885f0840b1359dd8d4c0a33f89ccbb614621da6
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-09-01 10:11:50 +0200

    Ränsade upp i solutionen.

--- Space Head/CursorAiming/Program.cs
+++ Space Head/CursorAiming/Program.cs
@@ -14,7 +14,7 @@ namespace CursorAiming
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new SpaceHeadGame())
                 game.Run();
         }
     }
--- Space Head/CursorAiming/SpaceHeadGame.cs
+++ Space Head/CursorAiming/SpaceHeadGame.cs
@@ -1,54 +1,53 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 
 namespace CursorAiming
 {
     /// <summary>
-    /// This is the main type for your game.
+    ///     This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class SpaceHeadGame : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private Vector2 distance;
+        private readonly GraphicsDeviceManager graphics;
 
-        Texture2D image;
-        Vector2 distance;
-        Vector2 spritePosition = new Vector2(500, 500);
-        
-        float rotation;
+        private Texture2D image;
+
+        private float rotation;
+        private SpriteBatch spriteBatch;
+        private Vector2 spritePosition = new Vector2(500, 500);
 
 
-
-        public Game1()
+        public SpaceHeadGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
         /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
+        ///     Allows the game to perform any initialization it needs to before starting to run.
+        ///     This is where it can query for any required services and load any non-graphic
+        ///     related content.  Calling base.Initialize will enumerate through any components
+        ///     and initialize them as well.
         /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            
+
             base.Initialize();
 
             graphics.PreferredBackBufferWidth = 1000;
             graphics.PreferredBackBufferHeight = 1000;
             graphics.ApplyChanges();
 
-            this.IsMouseVisible = true;
+            IsMouseVisible = true;
         }
 
         /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
+        ///     LoadContent will be called once per game and is the place to load
+        ///     all of your content.
         /// </summary>
         protected override void LoadContent()
         {
@@ -61,8 +60,8 @@ namespace CursorAiming
         }
 
         /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
+        ///     UnloadContent will be called once per game and is the place to unload
+        ///     game-specific content.
         /// </summary>
         protected override void UnloadContent()
         {
@@ -70,21 +69,22 @@ namespace CursorAiming
         }
 
         /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
+        ///     Allows the game to run logic such as updating the world,
+        ///     checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            MouseState mouse = Mouse.GetState();
+            var mouse = Mouse.GetState();
 
             distance.X = mouse.X - spritePosition.X;
             distance.Y = mouse.Y - spritePosition.Y;
 
-            rotation = (float)Math.Atan2(distance.Y, distance.X);
+            rotation = (float) Math.Atan2(distance.Y, distance.X);
 
 
             // TODO: Add your update logic here
@@ -93,7 +93,7 @@ namespace CursorAiming
         }
 
         /// <summary>
-        /// This is called when the game should draw itself.
+        ///     This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
@@ -102,7 +102,9 @@ namespace CursorAiming
 
             spriteBatch.Begin();
 
-            spriteBatch.Draw(image, new Rectangle((int)spritePosition.X, (int)spritePosition.Y, image.Width, image.Height), null, Color.White, rotation, new Vector2(image.Width / 2, image.Height / 2), SpriteEffects.None, 0);
+            spriteBatch.Draw(image,
+                new Rectangle((int) spritePosition.X, (int) spritePosition.Y, image.Width, image.Height), null,
+                Color.White, rotation, new Vector2(image.Width / 2, image.Height / 2), SpriteEffects.None, 0);
 
             spriteBatch.End();
 
