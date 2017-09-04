16a5cd8e20b6aea90184aafdfe17b6f8c06d3f42
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-09-04 11:48:42 +0200

    Lade till fiende som pekar på spelaren och skjuter.

--- Space Head/CursorAiming/Player.cs
+++ Space Head/CursorAiming/Player.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -11,88 +6,46 @@ namespace CursorAiming
 {
     internal class Player : UnitWithGun
     {
-       
-        
-            Texture = this.Game.Content.Load<Texture2D>("spaceAstronauts_009"); 
-            BulletTexture = this.Game.Content.Load<Texture2D>("laserBlue01");
-
         public Player(int _moveSpeed, int _bulletSpeed, int _bulletDamage, Game game) : base(game)
         {
             MoveSpeed = _moveSpeed;
             BulletSpeed = _bulletSpeed;
-            Texture = this.Game.Content.Load<Texture2D>("spaceAstronauts_009"); 
-            BulletTexture = this.Game.Content.Load<Texture2D>("laserBlue01");
-            BulletDamage = _bulletDamage;
-            Texture = this.Game.Content.Load<Texture2D>("spaceAstronauts_009");
-            BulletTexture = this.Game.Content.Load<Texture2D>("laserBlue01");
+            Texture = Game.Content.Load<Texture2D>("spaceAstronauts_009");
+            BulletTexture = Game.Content.Load<Texture2D>("laserBlue01");
         }
 
-        public override void UpdateGraphics(SpriteBatch spriteBatch)
-        public Player(int _moveSpeed, int _bulletSpeed, int _bulletDamage, Game game) : base(game)
+        protected override void LoadContent()
         {
-            spriteBatch.Draw(Texture,
-            BulletSpeed = _bulletSpeed;
-            BulletDamage = _bulletDamage;
-                new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height),
-                null, Color.White, Rotation, new Vector2(Texture.Width / 2, Texture.Height / 2), SpriteEffects.None, 0);
-            Texture = this.Game.Content.Load<Texture2D>("spaceAstronauts_009");
-            BulletTexture = this.Game.Content.Load<Texture2D>("laserBlue01");
+            Texture = Game.Content.Load<Texture2D>("spaceAstronauts_009");
+            BulletTexture = Game.Content.Load<Texture2D>("laserBlue01");
+
+            base.LoadContent();
         }
 
-        public Player(int _moveSpeed, int _bulletSpeed, int _bulletDamage, Game game) : base(game)
-        {
-            spriteBatch.Draw(Texture,
-            BulletSpeed = _bulletSpeed;
-            BulletDamage = _bulletDamage;
-            Texture = this.Game.Content.Load<Texture2D>("spaceAstronauts_009");
-            BulletTexture = this.Game.Content.Load<Texture2D>("laserBlue01");
-        }
-        public override void UpdateGraphics(SpriteBatch spriteBatch)
-        public Player(int _moveSpeed, int _bulletSpeed, int _bulletDamage, Game game) : base(game)
-        {
-            spriteBatch.Draw(Texture,
-            BulletSpeed = _bulletSpeed;
-            BulletDamage = _bulletDamage;
-            Texture = this.Game.Content.Load<Texture2D>("spaceAstronauts_009");
-            BulletTexture = this.Game.Content.Load<Texture2D>("laserBlue01");
-        }
+
         public override void UpdateGraphics(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(Texture,
-                new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height),
+                new Rectangle((int) Position.X, (int) Position.Y, Texture.Width, Texture.Height),
                 null, Color.White, Rotation, new Vector2(Texture.Width / 2, Texture.Height / 2), SpriteEffects.None, 0);
         }
-        public override void UpdateGraphics(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(Texture,
-                new Rectangle((int)Position.X, (int)Position.Y, Texture.Width, Texture.Height),
-                null, Color.White, Rotation, new Vector2(Texture.Width / 2, Texture.Height / 2), SpriteEffects.None, 0);
-        }
+
         public override void UpdateMovement(GameTime gameTime)
         {
             MoveDirection = new Vector2(0, 0);
             if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
                 MoveDirection.X += -1;
-            }
             if (Keyboard.GetState().IsKeyDown(Keys.D))
-            {
                 MoveDirection.X += 1;
-            }
             if (Keyboard.GetState().IsKeyDown(Keys.W))
-            {
                 MoveDirection.Y += -1;
-            }
             if (Keyboard.GetState().IsKeyDown(Keys.S))
-            {
                 MoveDirection.Y += 1;
-            }
- 
+
             if (MoveDirection.X != 0 && MoveDirection.Y != 0) MoveDirection.Normalize();
 
             Velocity = MoveDirection * (MoveSpeed * gameTime.ElapsedGameTime.Milliseconds / 1000);
             Position += Velocity;
         }
-        
     }
 }
