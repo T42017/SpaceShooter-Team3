3b2ac3d3b00b7a6df9e17b5c163a923954efcc7f
Rasmus Harrysson <rasmus.harrysson@lund.ntig.se>
2017-09-18 10:22:19 +0200

    Ändringar till bakgrunden

--- Space Head/CursorAiming/Globals.cs
+++ Space Head/CursorAiming/Globals.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CursorAiming
+﻿namespace CursorAiming
 {
-    class Globals
+    internal class Globals
     {
         public static int ScreenWidth = 1500;
         public static int ScreenHeight = 1100;
